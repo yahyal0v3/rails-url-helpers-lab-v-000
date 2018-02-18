d4187425adaca88f4a61616ab5f6f2886f7e55ac
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-18 21:17:13 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show]
+  resources :students, only: [:index, :show, :activate]
+  #get 'students/activate' to: ''
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
