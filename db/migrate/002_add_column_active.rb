0371af8b9fe91fad54a34ecb0d2918aa661e38c0
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-18 20:58:13 +0000

    automatically backed up by learn

--- db/migrate/002_add_column_active.rb
+++ db/migrate/002_add_column_active.rb
@@ -1,5 +1,5 @@
 class AddColumnActive < ActiveRecord::Migration
   def change
-    add_column :students, :active, boolean: default 
-  end 
+    add_column :students, :active, :boolean, default:
+  end
 end 
